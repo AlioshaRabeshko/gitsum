337a6809cc3a8f4a602babba425e2479bbed8805
Oleksiy Rabeshko <Leflylll@gmail.com>
2025-04-30 16:18:57 +0200

    wip

---
+++ esbuild.config.js
@@ -0,0 +1,24 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import esbuild from 'esbuild';
+
+esbuild
+	.build({
+		entryPoints: ['src/index.ts'],
+		bundle: true,
+		outdir: 'dist',
+		platform: 'node',
+		target: 'node14',
+		format: 'cjs',
+		minify: true,
+	})
+	.then(() => {
+		const distDir = path.join(path.dirname(new URL(import.meta.url).pathname), 'dist');
+		const filePath = path.join(distDir, 'index.js');
+		const newFilePath = filePath.replace('.js', '.cjs');
+		fs.renameSync(filePath, newFilePath);
+	})
+	.catch((error) => {
+		console.error('Error during build:', error);
+		process.exit(1);
+	});
---
+++ scripts/release.sh
@@ -0,0 +1,11 @@
+if [[ -n $(git status -s) ]]; then
+  echo "Commit repo first"
+  exit 1
+fi
+
+VERSION=v$(cat package.json | awk 'BEGIN{FS="\""}/"version"/{print $4}')
+
+cat .gitignore | awk '$0!~"dist"{print}' > ~.gitignore
+mv ~.gitignore .gitignore
+npm publish
+# git reset --hard origin/master
